114c61c6d9dbb7263764f3002108e69c58d610fd
anisirji <72395988+anisirji@users.noreply.github.com>
2021-02-25 09:41:22 +0530

    nav btn ready

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import Button from './components/button';
+
+
+
+ 
+function App() {
+  return (
+    <div className="App">
+     <Button label = "aniket"/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,10 @@
+function Button(props) {
+    return (
+      <div className="btn">
+        <span className="-u-btn">
+        {props.label}
+          </span>
+      </div>
+    );
+   }
+export default Button;
